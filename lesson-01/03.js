09284af96e41731535bbbd1fb2d10bc6e68bc8f4
YanaSmithDev <yana.kuznetsova.dev@gmail.com>
2024-05-22 01:25:21 +0300

    increment/ decrement

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -3,9 +3,9 @@ let number = 1;
 
 console.log(number); 
 number ++; 
-number ++;
-console.log(number); 
-console.log(number); 
+console.log(++number); 
+console.log(number++); 
+number--;
 number--;
 number--;
 console.log(number); 
