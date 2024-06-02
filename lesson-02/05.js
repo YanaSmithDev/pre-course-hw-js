82dfa9a28977d30fbf57fd123161ad808aef2cbc
YanaSmithDev <yana.kuznetsova.dev@gmail.com>
2024-06-02 18:17:16 +0300

    object clone

---
+++ lesson-02/05.js
@@ -0,0 +1,13 @@
+// code
+let passport = {
+  name: "Petr",
+  surname: "Petrov",
+};
+
+let clone = Object.assign({}, passport);
+
+clone.name = "Ivan";
+
+console.log(passport.name);
+console.log(clone.name);
+
