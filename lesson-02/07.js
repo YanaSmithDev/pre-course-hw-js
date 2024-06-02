9dda08dcdd6a9111ab94eeeca2ad570d1e6c5662
YanaSmithDev <yana.kuznetsova.dev@gmail.com>
2024-06-02 18:38:48 +0300

    deep clone 2

---
+++ lesson-02/07.js
@@ -0,0 +1,16 @@
+// code
+let passportMarried = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+      country: "USA",
+      city: "Bobryisk"
+  }
+};
+
+let passportMarried2 = structuredClone(passportMarried);
+passportMarried.married = true;
+
+console.log(passportMarried);
+console.log(passportMarried2);
+
