c425c160093b849bf2229120f29a006b629498f3
YanaSmithDev <yana.kuznetsova.dev@gmail.com>
2024-06-08 16:40:49 +0300

    array

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,5 @@
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 
-console.log(fruits.slice(1,-2));
+const newFruits = fruits.slice(1,-2);
+
+console.log(newFruits);
